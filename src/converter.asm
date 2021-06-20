860cf9f1fcbef83b01af95ebdab8160c394fa8d5
m-almeida0 <67078995+m-almeida0@users.noreply.github.com>
2021-06-20 15:27:23 -0300

    Update converter.asm

--- src/converter.asm
+++ src/converter.asm
@@ -5,19 +5,19 @@ numero: .asciiz "00000000000000000000000000000000"
 	.text
 	.globl imprimir_inteiro_na_base
 
-#$a0 deve conter o número a ser convertido, em decimal, e $a1 é a base para a qual ele deve ser convertido,
-#quando a função for chamada.
+#$a0 deve conter o nÃºmero a ser convertido, em decimal, e $a1 Ã© a base para a qual ele deve ser convertido,
+#quando a funÃ§Ã£o for chamada.
 imprimir_inteiro_na_base:
 	
 	addi $sp, $sp, -8
 	sw $a0, 0 ($sp)
 	sw $a1, 4 ($sp)
 
-	add $t0, $a0, $0#t0 é o número a ser convertido
-	add $t1, $a1, $0#t1 é a base à qual esse valor deve ser convertido
-	li $t2, 10#t2 é 10, pra ser usado num if mais pra frente
-	la $t3, numero#t3 é o endereço da string na qual o novo número vai ser impresso
-	addi $t3, $t3, 31#e ela é contada a partir do seu último caractere
+	addu $t0, $a0, $0#t0 Ã© o nÃºmero a ser convertido
+	add $t1, $a1, $0#t1 Ã© a base Ã  qual esse valor deve ser convertido
+	li $t2, 10#t2 Ã© 10, pra ser usado num if mais pra frente
+	la $t3, numero#t3 Ã© o endereÃ§o da string na qual o novo nÃºmero vai ser impresso
+	addi $t3, $t3, 31#e ela Ã© contada a partir do seu Ãºltimo caractere
 
 	loopResto:
 		div $t0, $t1
@@ -31,15 +31,15 @@ imprimir_inteiro_na_base:
 		addi $t4, $t4, 48
 	fimMaior10:
 
-		sb $t4, 0($t3)#salva t4 no último byte da string
-		addi $t3, $t3, -1#decrementa o endereço da string
+		sb $t4, 0($t3)#salva t4 no Ãºltimo byte da string
+		addi $t3, $t3, -1#decrementa o endereÃ§o da string
 
 		bgtz $t0, loopResto
 	fimLoop:
 	
-	addi $t3, $t3, 1#n loop de saida, e decrementado 1 de t3, o que faz com que o endereço armazenado seja uma unidade anterior a desejada
+	addi $t3, $t3, 1#n loop de saida, e decrementado 1 de t3, o que faz com que o endereÃ§o armazenado seja uma unidade anterior a desejada
 
-	add $s0, $t3, $0#armazena o endereco da string em s0
+	addu $s0, $t3, $0#armazena o endereco da string em s0
 	#parte de impressao de teste
 	add $a0, $0, $t3
 	li $v0, 4
